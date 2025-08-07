b0dbdc4edb842e1b8559c952bc071c041215ed3b
fauzaro01 <muhamadfauzan4750@gmail.com>
2025-08-07 10:01:39 +0700

    feat: update index route and view to conditionally render authentication links

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const {ensureAuthenticated} = require('../middleware/auth');
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        isAuth: req.isAuthenticated()
+    });
 })
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
